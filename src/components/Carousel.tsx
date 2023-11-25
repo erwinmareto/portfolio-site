10c9733ec70d98b66b726eac6faedf412238226a
Erwin Mareto Wikas <erwinmaretow@gmail.com>
2023-11-25 23:15:31 +0700

    fix: revamped carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -8,19 +8,13 @@ import { motion, AnimatePresence } from "framer-motion";
 const Carousel = () => {
   const [index, setIndex] = useState(0);
   const images = [first, sec, third, fourth];
-  const classes = [
-    "stopper",
-    "carousel-img-1",
-    "carousel-img-2",
-    "carousel-img-3",
-  ];
   const checkIndex = (number: number) => {
     if (number === images.length || number < 0) {
       return 0;
     }
     if (number < images.length) {
       console.log(index);
-      
+
       return number;
     }
     return number - images.length;
@@ -30,9 +24,11 @@ const Carousel = () => {
     move: {
       x: 0,
       y: 0,
+      opacity: 1,
+      transition: {
+        delay: 0.5,
+      },
     },
-    hidden: {opacity: 0},
-    visible: {opacity: 1}
   };
   const moveOne = {
     move: {
@@ -44,84 +40,72 @@ const Carousel = () => {
       },
     },
   };
-  const moveTwo = {
-    move: {
-      x: -20,
-      y: -20,
-      opacity: 1,
-      transition: {
-        delay: 0.5,
-      },
-    },
-  };
-  const moveThree = {
-    move: {
-      x: -30,
-      y: -30,
-      opacity: 1,
-      transition: {
-        type: 'easeIn'
-      },
-    },
-  };
+
   return (
     <motion.div
       className=" flex justify-center"
       variants={starter}
       animate="move"
       onClick={() => setIndex((index) => checkIndex((index += 1)))}
-
     >
-      <motion.img src={images[index]} className="static carousel-img-1 flex lg:hidden" />
       <AnimatePresence>
-        { index <= 0 && (
+        {index === 0 && (
           <motion.img
             src={first}
             width={500}
-            className={`${classes[index]} hidden lg:flex`}
-            initial={{opacity: 0}}
-            animate={{opacity: 1}}
-            exit={{ x: -300, opacity: 0 }}
+            className="project-img"
+            variants={moveOne}
+            initial={{ opacity: 0, x: 100 }}
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.9 }}
+            exit={{ x: -200, opacity: 0 }}
           />
         )}
       </AnimatePresence>
 
       <AnimatePresence>
-        {(index <= 1) && (
+        {index === 1 && (
           <motion.img
             src={sec}
             width={500}
-            className={`${classes.at(index - 1)} hidden lg:flex ${index === 0 && 'relative right-0'}`}
+            className="project-img"
             variants={moveOne}
-            initial={{opacity: 0}}
-            exit={{ x: -300, opacity: 0 }}
+            initial={{ opacity: 0, x: 100 }}
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.9 }}
+            exit={{ x: -200, opacity: 0 }}
           />
         )}
       </AnimatePresence>
 
       <AnimatePresence>
-      { (index <= 2) && <motion.img
-        src={third}
-        width={500}
-        className={`${classes.at(index - 2)} hidden lg:flex ${index === 1 && 'relative right-0'}`}
-        variants={moveTwo}
-        initial={{opacity: 0}}
-        exit={{ x: -300, opacity: 0 }}
-      />}
+        {index === 2 && (
+          <motion.img
+            src={third}
+            width={500}
+            className="project-img"
+            variants={moveOne}
+            initial={{ opacity: 0, x: 100 }}
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.9 }}
+            exit={{ x: -200, opacity: 0 }}
+          />
+        )}
       </AnimatePresence>
 
       <AnimatePresence>
-        {(index >= 2) && 
-      <motion.img
-        src={fourth}
-        width={500}
-        className={`${classes.at(index - 3)} hidden lg:flex ${'relative right-0'}`}
-        variants={moveThree}
-        initial={{opacity: 0}}
-        animate='move'
-        exit={{ x: -300, opacity: 0}}
-      />
-        }
+        {index === 3 && (
+          <motion.img
+            src={fourth}
+            width={500}
+            className="project-img"
+            variants={moveOne}
+            initial={{ opacity: 0, x: 100 }}
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.9 }}
+            exit={{ x: -200, opacity: 0 }}
+          />
+        )}
       </AnimatePresence>
     </motion.div>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -13,7 +13,7 @@ const Projects = () => {
     "TailwindCSS",
     "ExpressJS",
     "PrismaORM",
-    "PostgreSQL"
+    "PostgreSQL",
   ];
 
   const starter = {
@@ -46,59 +46,62 @@ const Projects = () => {
   };
   return (
     <section id="projects-section" className="projects mx-auto py-10">
-      <div className="container">
-
-        <motion.h1
-          className="title title-bg text-6xl lg:text-8xl"
-          initial={{ x: -200, opacity: 0 }}
-          whileInView={{ x: 0, opacity: 1 }}
-          viewport={{ once: true }}
-          transition={{
-            type: "easeIn",
-            duration: 2,
-          }}
-        >
-          Projects
-        </motion.h1>
-
-        <div className="flex flex-col my-10">
-          <div className="grid">
-            <Carousel />
+      <div className="flex justify-center">
+        <div className="container">
+          <div className="title title-bg">
+            <motion.h1
+              className="text-6xl lg:text-8xl"
+              initial={{ x: -200, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{
+                type: "easeIn",
+                duration: 2,
+              }}
+            >
+              Projects
+            </motion.h1>
           </div>
-          <article className="flex-col text-center" ref={ref}>
-            <motion.p
-              className="text-5xl mt-10"
-              initial={{ y: 100, opacity: 0 }}
-              whileInView={{ y: 0, opacity: 1 }}
-              viewport={{ once: true }}
-              transition={{ type: "easeIn" }}
-            >
-              Lorem ipsum
-            </motion.p>
-            <motion.ul
-              variants={starter}
-              initial="hidden"
-              whileInView="appear"
-              viewport={{ once: true, amount: 0.8 }}
-              className="badge-list"
-            >
-              {wihireStack.map((title) => (
-                <motion.li className="badge" variants={items}>
-                  {title}
-                </motion.li>
-              ))}
-            </motion.ul>
-            <motion.p
-              className="text-2xl font-['poppins'] mt-5"
-              initial={{ y: 100, opacity: 0 }}
-              whileInView={{ y: 0, opacity: 1 }}
-              viewport={{ once: true }}
-              transition={{ type: "easeIn" }}
-            >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-              sit incidunt earum asasaas
-            </motion.p>
-          </article>
+
+          <div className="flex flex-col my-10">
+            <div className="grid">
+              <Carousel />
+            </div>
+            <article className="flex-col text-center" ref={ref}>
+              <motion.p
+                className="text-5xl mt-10"
+                initial={{ y: 100, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{ type: "easeIn" }}
+              >
+                Lorem ipsum
+              </motion.p>
+              <motion.ul
+                variants={starter}
+                initial="hidden"
+                whileInView="appear"
+                viewport={{ once: true, amount: 0.8 }}
+                className="badge-list"
+              >
+                {wihireStack.map((title) => (
+                  <motion.li className="badge" variants={items}>
+                    {title}
+                  </motion.li>
+                ))}
+              </motion.ul>
+              <motion.p
+                className="text-2xl font-['poppins'] mt-5"
+                initial={{ y: 100, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                viewport={{ once: true }}
+                transition={{ type: "easeIn" }}
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+                sit incidunt earum asasaas
+              </motion.p>
+            </article>
+          </div>
         </div>
       </div>
       <Skills />
